e7573f85b293d3efc331ac5970cab4131327f7a0
EliotWork <bereznev68@mail.ru>
2022-10-30 17:12:03 +0500

    create jobShedular

--- app/src/main/java/com/example/servicetest/service/MyJobService.kt
+++ app/src/main/java/com/example/servicetest/service/MyJobService.kt
@@ -5,12 +5,14 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.*
 import java.security.Provider
 
-class MyJobService: JobService() {
+class MyJobService : JobService() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
@@ -21,13 +23,24 @@ class MyJobService: JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
+
         return true
     }
 
@@ -49,5 +62,12 @@ class MyJobService: JobService() {
     companion object {
 
         const val JOB_ID = 111
+        private const val PAGE = "page"
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
+
     }
 }
